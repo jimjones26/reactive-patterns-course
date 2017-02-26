76dafc0cad7cefcc63d4c6babfbebc998b11dd54
jimjones26 <jimjones26@gmail.com>
2017-02-26 17:07:45 -0500

    scaffold lessons list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { BrowserEventExperimentsComponent } from './browser-event-experiments/browser-event-experiments.component';
 import { EventBusExperimentsComponent } from './event-bus-experiments/event-bus-experiments.component';
+import { LessonsListComponent } from './lessons-list/lessons-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BrowserEventExperimentsComponent,
-    EventBusExperimentsComponent
+    EventBusExperimentsComponent,
+    LessonsListComponent
   ],
   imports: [
     BrowserModule,
